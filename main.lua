e6d828f12d53b21955d96fea83dd088a39131855
Mega <eowjd112500@gmail.com>
2024-11-22 23:31:42 +0900

    Update and rename Main.lua to main.lua

--- main.lua
+++ main.lua
@@ -19,3 +19,21 @@ w:Toggle("Delete Doors", function(v)
         end
     end
 end)
+
+w:Button("Get AK47", function()
+local ak47 = workspace:WaitForChild("Prison_ITEMS"):WaitForChild("giver"):WaitForChild("AK-47"):WaitForChild("ITEMPICKUP")
+
+local player = game.Players.LocalPlayer
+local originalPosition = player.Character.HumanoidRootPart.Position
+local ak47Position = ak47.Position
+
+player.Character:SetPrimaryPartCFrame(CFrame.new(ak47Position))
+wait(1)
+
+workspace:WaitForChild("Remote"):WaitForChild("ItemHandler"):InvokeServer(ak47)
+wait(1)
+
+local offset = Vector3.new(0, 5, 0)
+player.Character:SetPrimaryPartCFrame(CFrame.new(originalPosition + offset))
+
+end)
