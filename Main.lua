5f014b7c543ced5b0a89a3778015f125b429622b
Mega <eowjd112500@gmail.com>
2024-11-22 20:28:33 +0900

    Create Main.lua

---
+++ Main.lua
@@ -0,0 +1,14 @@
+𝗦𝗰𝗿𝗶𝗽𝘁local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/lime"))()
+local w = Library:Window("Mega Hub")
+
+local doorsBackup
+
+w:Toggle("Delete Doors", function(v)
+    if v and workspace:FindFirstChild("Doors") then
+        doorsBackup = workspace.Doors:Clone()
+        workspace.Doors:Destroy()
+    elseif doorsBackup then
+        doorsBackup.Parent = workspace
+        doorsBackup = nil
+    end
+end)
